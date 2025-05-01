6ca2543e74611347922dbf2e34b468ff3196ee83
Inácio <88351922+reysmalldev@users.noreply.github.com>
2025-05-01 11:02:58 -0300

    Add tests to User model

--- test/fixtures/user_test.rb
+++ test/fixtures/user_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Invalid User record should not be saved" do
+    user = User.new
+
+    assert_not user.save
+  end
+
+  test "Valid User record should be saved" do
+    user = User.new(email_address: "example@gmail.com", password: "supersecurepassword123", password_confirmation: "supersecurepassword123")
+
+    assert true, user.save
+  end
 end
