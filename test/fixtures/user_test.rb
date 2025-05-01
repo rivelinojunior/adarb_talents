d4fc32103c3018ec4d2e8ae9d8058ef81dea8428
Inácio <88351922+reysmalldev@users.noreply.github.com>
2025-05-01 13:29:54 -0300

    Fix: correct tests and queue database_path in development env

--- test/fixtures/user_test.rb
+++ test/fixtures/user_test.rb
@@ -10,6 +10,6 @@ class UserTest < ActiveSupport::TestCase
   test "Valid User record should be saved" do
     user = User.new(email_address: "example@gmail.com", password: "supersecurepassword123", password_confirmation: "supersecurepassword123")
 
-    assert true, user.save
+    assert user.save
   end
 end
