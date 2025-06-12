42624bb739416ab9c98cb09d8cc04f0e9dea4367
Jose Rafael Camejo <jose20camejo@gmail.com>
2025-06-12 14:44:58 -0300

    add docker configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,14 +31,18 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Configure Action Mailer to use MailHog
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch("SMTP_ADDRESS") {
+      "localhost"
+    },
+    port: ENV.fetch("SMTP_PORT") { 1025 }.to_i
+  }
+  config.action_mailer.raise_delivery_errors = true # Recomendado para desarrollo para ver errores
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
-
-  # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
